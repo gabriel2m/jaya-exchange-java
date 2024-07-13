0309fe6c9e18cd35fe11e96b785ee969be19f115
gabriel2m <gabriel2m.contact@gmail.com>
2024-07-13 13:35:32 -0300

    feat: add transactions store endpoint

---
+++ src/main/java/com/gabriel2m/exchange/controller/TransactionController.java
@@ -0,0 +1,44 @@
+package com.gabriel2m.exchange.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.gabriel2m.exchange.repository.TransactionRepository;
+import com.gabriel2m.exchange.model.Transaction;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/transactions")
+public class TransactionController {
+  	private final TransactionRepository transactionRepository;
+
+	@PostMapping
+	public Transaction store(
+		@RequestBody Transaction transaction,
+		@Value("${exchange_rates_api.access_key}") String accessKey,
+		@Value("${exchange_rates_api.base_url}") String baseUrl,
+		@Value("${exchange_rates_api.base_currency}") String baseCurrency
+	) {
+		RestTemplate restTemplate = new RestTemplate();
+
+		JsonNode rates = restTemplate.getForObject(
+			baseUrl+"?base="+baseCurrency+"&access_key="+accessKey,
+			JsonNode.class
+		).get("rates");
+
+		double from = rates.get(transaction.getFrom()).asDouble();
+		double to = rates.get(transaction.getTo()).asDouble();
+
+		transaction.setRate(to/from);
+		
+		return transactionRepository.save(transaction);
+	}
+}
